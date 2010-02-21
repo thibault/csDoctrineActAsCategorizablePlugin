47bb058ddb64e45cb58b145600640bef6f5fb157
Thibault Jouannic <teeboo@gmail.com>
2010-02-21 16:07:38 +0100

    retab file

--- lib/template/Categorizable.php
+++ lib/template/Categorizable.php
@@ -13,15 +13,15 @@ class Doctrine_Template_Categorizable extends Doctrine_Template
    * Array of Categorizable options
    */
   protected $_options = array('category' => array(
-                                'model'		=> 'Category',
+                                'model'   => 'Category',
                                 'alias'   => 'Categories',
                                 'foreignAlias'   =>  null,
                                 'refClass' => null,
                                 'local' => null,
                                 'foreign' => 'category_id',
                               ),
-															'root'				=>  null,
-	);
+                              'root'        =>  null,
+  );
 
 
   /**
@@ -76,82 +76,82 @@ class Doctrine_Template_Categorizable extends Doctrine_Template
     ));
   }
 
-	public function createRootTableProxy()
-	{
-		$root_category = new Category();
-		$root_category['name'] = $this->_options['root'] ? $this->_options['root'] : get_class($this->getInvoker()).'_Root';
-		$root_category->save();
-		$root_category->getTable()->getTree()->createRoot($root_category);
-		return $root_category;
-	}
+  public function createRootTableProxy()
+  {
+    $root_category = new Category();
+    $root_category['name'] = $this->_options['root'] ? $this->_options['root'] : get_class($this->getInvoker()).'_Root';
+    $root_category->save();
+    $root_category->getTable()->getTree()->createRoot($root_category);
+    return $root_category;
+  }
 
-	public function addCategoryTableProxy($category)
-	{
-		if(!$category->getNode()->getRootValue());
-		{
-			$root_name = $this->_options['root'] ? $this->_options['root'] : get_class($this->getInvoker()).'_Root';
-			$this_root = Doctrine::getTable('Category')->getRootByClassName($root_name);
+  public function addCategoryTableProxy($category)
+  {
+    if(!$category->getNode()->getRootValue());
+    {
+      $root_name = $this->_options['root'] ? $this->_options['root'] : get_class($this->getInvoker()).'_Root';
+      $this_root = Doctrine::getTable('Category')->getRootByClassName($root_name);
 
-			if(!$this_root)
-			{
-				$this_root = $this->createRootTableProxy();
-			}
-			$this_root->getNode()->addChild($category);
-			$category->refresh();
-		}
-	}
-	public function addCategory($category)
-	{
-		$this->addCategoryTableProxy($category);
-		$this->setCategory($category);
-	}
-	public function removeCategoryTableProxy($category)
-	{
-		$category->getNode()->deleteNode();
-	}
-	public function getCategoryTreeTableProxy()
-	{
-		$name = $this->_options['root'] ? $this->_options['root'] : get_class($this->getInvoker()).'_Root';
-		return Doctrine::getTable('Category')->getCategoryTree($name);
-	}
-	public function getCategoriesQueryTableProxy()
-	{
-		$tree = $this->getCategoryTreeTableProxy();
-		$root = $tree->fetchRoot();
-		$q = Doctrine::getTable('Category')->createQuery();
-		if($root)
-		{
-			$q->where('root_id = ? AND level != 0', $root->getId());
-		}
-		return $q;
-	}
-	public function findAllByCategoryTableProxy($name)
-	{
-		$category = Doctrine::getTable('Category')->getCategory($name);
-		return $this->getInvoker()->getTable()->createQuery()->where('category_id = ?', $category->getId())->execute();
-	}
-	public function findAllByCategorySlugTableProxy($slug)
-	{
-		$category = Doctrine::getTable('Category')->getCategory($slug);
-		return $this->getInvoker()->getTable()->createQuery()->where('category_id = ?', $category->getId())->execute();
-	}
-	public function getCategoriesTableProxy()
-	{
-		return $this->getCategoriesQueryTableProxy()->execute();
-	}
-	public function setCategory($category)
-	{
-		if($category instanceof Category)
-		{
-			$category->secureSave();
-			return $this->getInvoker()->setCategory($category);
-		}
-		elseif(is_string($category))
-		{
-			$category->secureSave();
-			$category = Doctrine::getTable('Category')->getOrCreateCategory($category);
-			return $this->getInvoker()->setCategory($category);
-		}
-		throw new sfException("Parameters passed to setSubCategory must be an instance of Category or type string");
-	}
+      if(!$this_root)
+      {
+        $this_root = $this->createRootTableProxy();
+      }
+      $this_root->getNode()->addChild($category);
+      $category->refresh();
+    }
+  }
+  public function addCategory($category)
+  {
+    $this->addCategoryTableProxy($category);
+    $this->setCategory($category);
+  }
+  public function removeCategoryTableProxy($category)
+  {
+    $category->getNode()->deleteNode();
+  }
+  public function getCategoryTreeTableProxy()
+  {
+    $name = $this->_options['root'] ? $this->_options['root'] : get_class($this->getInvoker()).'_Root';
+    return Doctrine::getTable('Category')->getCategoryTree($name);
+  }
+  public function getCategoriesQueryTableProxy()
+  {
+    $tree = $this->getCategoryTreeTableProxy();
+    $root = $tree->fetchRoot();
+    $q = Doctrine::getTable('Category')->createQuery();
+    if($root)
+    {
+      $q->where('root_id = ? AND level != 0', $root->getId());
+    }
+    return $q;
+  }
+  public function findAllByCategoryTableProxy($name)
+  {
+    $category = Doctrine::getTable('Category')->getCategory($name);
+    return $this->getInvoker()->getTable()->createQuery()->where('category_id = ?', $category->getId())->execute();
+  }
+  public function findAllByCategorySlugTableProxy($slug)
+  {
+    $category = Doctrine::getTable('Category')->getCategory($slug);
+    return $this->getInvoker()->getTable()->createQuery()->where('category_id = ?', $category->getId())->execute();
+  }
+  public function getCategoriesTableProxy()
+  {
+    return $this->getCategoriesQueryTableProxy()->execute();
+  }
+  public function setCategory($category)
+  {
+    if($category instanceof Category)
+    {
+      $category->secureSave();
+      return $this->getInvoker()->setCategory($category);
+    }
+    elseif(is_string($category))
+    {
+      $category->secureSave();
+      $category = Doctrine::getTable('Category')->getOrCreateCategory($category);
+      return $this->getInvoker()->setCategory($category);
+    }
+    throw new sfException("Parameters passed to setSubCategory must be an instance of Category or type string");
+  }
 }
