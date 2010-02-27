f0155e178984fe656091c534b394bd4e2b260644
Thibault Jouannic <teeboo@gmail.com>
2010-02-27 19:03:16 +0100

    fixtures : clean old model files

--- test/fixtures/project/data/sql/schema.sql
+++ test/fixtures/project/data/sql/schema.sql
@@ -1,4 +1,3 @@
-CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(255));
 CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), root_id INTEGER, lft INTEGER, rgt INTEGER, level INTEGER);
 CREATE TABLE category_object (category_id INTEGER, categorized_model VARCHAR(50), categorized_id INTEGER, PRIMARY KEY(category_id, categorized_model, categorized_id));
 CREATE TABLE test_article (id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(255));
--- test/fixtures/project/lib/model/doctrine/ArticleTable.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class ArticleTable extends Doctrine_Table
-{
-}
