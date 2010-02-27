699d700576c692c4789818d78a5ca3c96e17bd79
Thibault Jouannic <teeboo@gmail.com>
2010-02-27 13:55:48 +0100

    add test unit file for template

---
+++ test/unit/template/Doctrine_Template_CategorizableTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Doctrine_Template_Categorizable tests.
+ */
+include_once dirname(__FILE__).'/../../bootstrap/unit.php';
+
+LimeAnnotationSupport::enable();
+$t = new lime_test(0);
+
+// @Before
+$article = new TestArticle();
+$article->setTitle('my test article');
+$article->save();
+
+// @After
+$article->delete();
+
+// @Test: Template is available
+
+$t->ok($article->getTable()->hasRelation('Categories'));
