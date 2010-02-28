e63359e395730a8c4211db314ed3b6dc5b635264
Thibault Jouannic <teeboo@gmail.com>
2010-02-28 19:23:52 +0100

    replace doctrine relation by a fake one
    As we can't override the refClass creation when the object is saved, we
    have to remove the doctrine relation, and fake it with custom codes in
    hooks.

--- lib/listener/Categorizable.php
+++ lib/listener/Categorizable.php
@@ -5,56 +5,51 @@
  *
  * @package csDoctrineActAsCategorizablePlugin
  * @subpackage listener
+ * @author Thibault Jouannic <thibault@jouannic.fr>
  **/
 class Doctrine_Template_Listener_Categorizable extends Doctrine_Record_Listener
 {
   /**
-   * Array of Categorizable options
-   */
-  protected $_options = array();
-
-  /**
    * We take care of the categories reference here
+   * We simulate a many to many relation
    *
    * @see Doctrine_Connection_UnitOfWork::saveAssociations()
    * @see Doctrine_Record::preSave()
    **/
-  public function preSave(Doctrine_Event $event)
+  public function postSave(Doctrine_Event $event)
   {
     $invoker = $event->getInvoker();
+    $categories = $invoker->getCategories();
 
-    $ref = $invoker->reference('Categories');
-    if ($ref)
+    if($categories instanceOf Doctrine_Collection)
     {
-      $rel = $invoker->getTable()->getRelation('Categories');
-
-      if ($rel instanceof Doctrine_Relation_Association)
+      // Associations deletions
+      foreach ($categories->getDeleteDiff() as $c)
       {
-        $assocTable = $rel->getAssociationTable();
-
-        // Associations deletions
-        foreach ($ref->getDeleteDiff() as $r)
-        {
-          Doctrine::getTable('CategoryObject')->createQuery()
-            ->delete()
-            ->addWhere('category_id = ?', $r->getId())
-            ->addWhere('categorized_id = ?', $invoker->getId())
-            ->addWhere('categorized_model = ?', get_class($invoker))
-            ->execute();
-        }
-
-        // Take care of new associations
-        foreach ($ref->getInsertDiff() as $r)
-        {
-          $r->save();
-          $assocRecord = $assocTable->create();
-          $assocRecord->set('category_id', $r->getId());
-          $assocRecord->set('categorized_id', $invoker->getId());
-          $assocRecord->set('categorized_model', get_class($invoker));
-          $assocRecord->save();
-          $ref->takeSnapshot();
-        }
+        $q = Doctrine::getTable('CategoryObject')->createQuery()
+          ->delete()
+          ->addWhere('category_id = ?', $c->getId())
+          ->addWhere('categorized_id = ?', $invoker->getId())
+          ->addWhere('categorized_model = ?', get_class($invoker));
+          $q->execute();
       }
+
+      // Take care of new associations
+      foreach ($categories->getInsertDiff() as $c)
+      {
+        // This is the added category
+        $c->save();
+
+        // And this is the refClass object
+        $assocRecord = new CategoryObject();
+        $assocRecord->set('category_id', $c->getId());
+        $assocRecord->set('categorized_id', $invoker->getId());
+        $assocRecord->set('categorized_model', get_class($invoker));
+        $assocRecord->save();
+      }
+
+      // Take a snapshot to record the current collection state
+      $categories->takeSnapshot();
     }
   }
 
@@ -72,5 +67,7 @@ class Doctrine_Template_Listener_Categorizable extends Doctrine_Record_Listener
       ->addWhere('categorized_id = ?', $invoker->getId())
       ->addWhere('categorized_model = ?', get_class($invoker))
       ->execute();
+
+    $invoker->setCategories(null);
   }
 }
--- lib/template/Categorizable.php
+++ lib/template/Categorizable.php
@@ -15,15 +15,8 @@ class Doctrine_Template_Categorizable extends Doctrine_Template
   /**
    * Array of Categorizable options
    */
-  protected $_options = array('model'   => 'Category',
-                              'alias'   => 'Categories',
-                              'foreignAlias'   =>  null,
-                              'refClass' => 'CategoryObject',
-                              'local' => 'categorized_id',
-                              'foreign' => 'category_id',
-                              'root'        =>  null,
-  );
-
+  protected $_options = array();
+  protected $_categories = null;
 
   /**
    * Constructor for Categorizable Template
@@ -42,20 +35,10 @@ class Doctrine_Template_Categorizable extends Doctrine_Template
   public function setTableDefinition()
   {
     $this->addListener(new Doctrine_Template_Listener_Categorizable($this->_options));
-  }
 
-  /**
-   * Initialize the category relationship
-   **/
-  public function setUp()
-  {
-    $relation = sprintf('%s as %s', $this->_options['model'], $this->_options['alias']);
-
-    $this->hasMany($relation, array(
-      'refClass' => $this->_options['refClass'],
-      'local' => $this->_options['local'],
-      'foreign' => $this->_options['foreign']
-    ));
+    // We manage categories as a virtual field
+    // Usual doctrine association magic is simulated in the listener
+    $this->getInvoker()->hasAccessorMutator('Categories', 'getCategories', 'setCategories');
   }
 
   /**
@@ -68,98 +51,34 @@ class Doctrine_Template_Categorizable extends Doctrine_Template
     $q = Doctrine_Query::create()
       ->select('c.*')
       ->from('Category c')
-      ->andwhere('c.categorized_type = ?', get_class($this->getInvoker()))
-      ->andWhere('c.categorized_id = ?', $this->getInvoker()->getId());
+      ->leftJoin('c.CategoryObject co')
+      ->addwhere('co.categorized_model = ?', get_class($this->getInvoker()))
+      ->addWhere('co.categorized_id = ?', $this->getInvoker()->getId());
 
     return $q;
   }
 
   /**
    * Return a collection of categories the invoker belongs to
+   * This will be the default accessor for the Categories relation
    *
    * @return Doctrine_Collection
    **/
   public function getCategories()
   {
-    return $this->getCategoriesQuery()->execute();
+    // I cannot imagine a case where this is incorrect, but let's keep an eye on this
+    if (null === $this->_categories)
+    {
+      $this->setCategories($this->getCategoriesQuery()->execute());
+    }
+    return $this->_categories;
   }
 
-  public function createRootTableProxy()
+  /**
+   * Custom mutator for categories
+   **/
+  public function setCategories(Doctrine_Collection $categories = null)
   {
-    $root_category = new Category();
-    $root_category['name'] = $this->_options['root'] ? $this->_options['root'] : get_class($this->getInvoker()).'_Root';
-    $root_category->save();
-    $root_category->getTable()->getTree()->createRoot($root_category);
-    return $root_category;
-  }
-
-  public function addCategoryTableProxy($category)
-  {
-    if(!$category->getNode()->getRootValue());
-    {
-      $root_name = $this->_options['root'] ? $this->_options['root'] : get_class($this->getInvoker()).'_Root';
-      $this_root = Doctrine::getTable('Category')->getRootByClassName($root_name);
-
-      if(!$this_root)
-      {
-        $this_root = $this->createRootTableProxy();
-      }
-      $this_root->getNode()->addChild($category);
-      $category->refresh();
-    }
-  }
-  public function addCategory($category)
-  {
-    $this->addCategoryTableProxy($category);
-    $this->setCategory($category);
-  }
-  public function removeCategoryTableProxy($category)
-  {
-    $category->getNode()->deleteNode();
-  }
-  public function getCategoryTreeTableProxy()
-  {
-    $name = $this->_options['root'] ? $this->_options['root'] : get_class($this->getInvoker()).'_Root';
-    return Doctrine::getTable('Category')->getCategoryTree($name);
-  }
-  public function getCategoriesQueryTableProxy()
-  {
-    $tree = $this->getCategoryTreeTableProxy();
-    $root = $tree->fetchRoot();
-    $q = Doctrine::getTable('Category')->createQuery();
-    if($root)
-    {
-      $q->where('root_id = ? AND level != 0', $root->getId());
-    }
-    return $q;
-  }
-  public function findAllByCategoryTableProxy($name)
-  {
-    $category = Doctrine::getTable('Category')->getCategory($name);
-    return $this->getInvoker()->getTable()->createQuery()->where('category_id = ?', $category->getId())->execute();
-  }
-  public function findAllByCategorySlugTableProxy($slug)
-  {
-    $category = Doctrine::getTable('Category')->getCategory($slug);
-    return $this->getInvoker()->getTable()->createQuery()->where('category_id = ?', $category->getId())->execute();
-  }
-  public function getCategoriesTableProxy()
-  {
-    return $this->getCategoriesQueryTableProxy()->execute();
-  }
-  public function setCategory($category)
-  {
-    if($category instanceof Category)
-    {
-      $category->secureSave();
-      return $this->getInvoker()->setCategory($category);
-    }
-    elseif(is_string($category))
-    {
-      $category->secureSave();
-      $category = Doctrine::getTable('Category')->getOrCreateCategory($category);
-      return $this->getInvoker()->setCategory($category);
-    }
-    throw new sfException("Parameters passed to setSubCategory must be an instance of Category or type string");
+    $this->_categories = $categories;
   }
 }
